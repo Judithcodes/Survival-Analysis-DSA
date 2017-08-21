3402a9e2b4c258b881037b18aafedc20cd1791d9
Michael Lipski <mvl5023@psu.edu>
2017-08-21 16:22:49 -0400

    Code updated, spectrum_occ function added

--- Hall_DSA.m
+++ Hall_DSA.m
@@ -6,12 +6,30 @@
 
 % Training algorithm with spectrum occupancy data representative of channel
 % characteristics
-length = 100;
-trainer = spectrum_occ(1, length);
-counts = zeros(1, length);    % stores number of occurences of each idle period length
-t = 0;
-tau = 1;         % transmit duration requested
 
+% Simulation variables
+length = 100;                    % number of samples in each channel of spectrum occupancy data
+channels = 10;                   % number of channels in test occupancy matrix
+counts = zeros(1, length);    % stores number of occurences of each idle period length
+t = 0;                            % time marker
+tau = 1;                          % transmit duration requested
+
+% Occupancy data
+trainer = spectrum_occ(1, length);          % training array for DSA algorithm
+M = spectrum_occ(channels, length);         % test matrix of occupancy data
+occupied = sum(M, 2);
+vacant = length - occupied;
+
+% Secondary user transmit request scheduling
+duty2nd = 10;                     % duty cycle for secondary user transmit
+period2nd = 10;                   % period for secondary user transmit
+schedule2nd = [zeros(1, 100-duty2nd), ones(1, duty2nd)];
+schedule2nd = repmat(schedule2nd, 1, length/period);     % transmit schedule for secondary user
+decision = zeros(channels, length/period2nd);
+
+%--------------------------------------------------------------------------------------------------
+% Train DSA algorithm
+%--------------------------------------------------------------------------------------------------
 for i = 1:length
     if trainer(i) == 0
         t = t+1;
@@ -27,9 +45,28 @@ for i = 1:length
     end
 end
 
+% Calculate survival/hazard function
 periodsIdle = sum(counts);
 pdf = counts./periodsIdle;
+cdf = zeros(1, length);
 
 for i = 1:length
-    cdf(i) = sum(pdf(1:i));
+    cdf(i) = sum(pdf(i:length));
+end
+
+% Scan test matrix of occupancy data and grant or deny transmission
+% requests
+for i = 1:channels
+    t = 0;
+    for j = 1:samples
+        sample = M(i, j);
+        if sample == 0
+            t = t + 1;
+            if schedule2nd == 1
+                
+            end
+        elseif sample == 1
+            t = 0;
+        end
+    end
 end
